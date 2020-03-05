fab887dd116c3670e39fac915146e0513260a1da
tommyn707 <tommy.n707@gmail.com>
2020-03-04 20:35:02 -0800

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+const RegisterForm = props => {
+    const [ formState, setFormState ] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
+    const onChangeHandler = event => {
+        setFormState({
+            ...formState,
+            [event.target.name]: event.target.value
+        });
+    }
+    const onSubmitHandler = event => {
+        event.preventDefault();
+        console.log(formState);
+    }
+    return(
+        <div>
+            <form onSubmit={onSubmitHandler}>
+                <label>First Name</label>
+                <input type="text" name="firstName" onChange={onChangeHandler}/>
+                <br/>        
+                <label>Last Name</label>
+                <input type="text" name="lastName" onChange={onChangeHandler}/>
+                <br/>        
+                <label>Email</label>
+                <input type="email" name="email" onChange={onChangeHandler}/>
+                <br/>        
+                <label>Password</label>
+                <input type="password" name="password" onChange={onChangeHandler}/>
+                <br/>        
+                <label>Confirm Password</label>
+                <input type="password" name="confirmPassword" onChange={onChangeHandler}/>
+                <br/>   
+                <input type="submit" />     
+            </form>
+            <div>
+            <p>Hello {formState.firstName} {formState.lastName}</p>
+            <p>email: {formState.email}</p>
+            <p>password: {formState.password}</p>
+            <p>confirm password: {formState.confirmPassword}</p>
+            </div>
+        </div>
+    );
+}
+
+export default RegisterForm;
